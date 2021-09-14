748f0c6ed06204303de3496c3af46ecd90458cb0
Reagin Turner <rturnerafteragnes@gmail.com>
2021-09-14 09:58:01 -0400

    TAdded password length prompts

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Code I added 
+//Code I added to store the characters
 const specialCharacters= "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 const lowerCase = "abcdefghijklmnopqrstuvwxyz"
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers = "123456789"
 
+// Code added by me
+
+function generatePassword() {
+  var passwordLength = prompt("Please choose the length of your password. (Must be 8 - 128 characters long)")
+  if (passwordLength >= 8 && passwordLength < 129) {
+    document.querySelector("#password").value //How to store values??
+    }
+  else {
+    var retry = alert("Password must be 8-128 characters.")
+    retry.textContent.appendChild
+    return null;
+    }
+
+  
+  }
+
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(event) {
+  event.preventDefault(); //Added to stop the generate button from doing default.
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-//Code I added 
-
-var length = password.length
-
-function generatePassword() {
-  prompt("Please choose the length of your password. (Must be 8 - 128 characters long)")
-  if (length < 8) {
-    alert("Password must be at least 8 characters")
-    return null;
-    }
-  else if (length > 128) {
-    alert("Password must be less than 129 characters")
-    return null;
-    }
-  }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Code I added
-//for generatePassword
-
-generatePassword();
