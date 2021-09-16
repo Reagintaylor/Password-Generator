a6e07ad2ccc46c52e15e4b8104761ba37e4a3eef
Reagin Turner <rturnerafteragnes@gmail.com>
2021-09-15 23:21:33 -0400

    Tried my best to finish, got super stuck

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Password-Generator
+## Password-Generator
+
+# Description
+- This project will demonstrate how to generate a random password using various criteria such as; the length of the password, and whether or not to include upper and lowercase numbers, special characters and numbers. The user will determine this through a series of prompts. This project will teach me how to apply various JavaScript fundamentals. 
+
+# Installation
+- To install this project, simply open up the link in your browser and click "Generate Paassword" to get started.
+
+# 
--- script.js
+++ script.js
@@ -7,23 +7,6 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz"
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers = "123456789"
 
-// Code added by me
-
-function generatePassword() {
-  var passwordLength = prompt("Please choose the length of your password. (Must be 8 - 128 characters long)")
-  if (passwordLength >= 8 && passwordLength < 129) {
-    document.querySelector("#password").value //How to store values??
-    }
-  else {
-    var retry = alert("Password must be 8-128 characters.")
-    retry.textContent.appendChild
-    return null;
-    }
-
-  
-  }
-
-
 // Write password to the #password input
 function writePassword(event) {
   event.preventDefault(); //Added to stop the generate button from doing default.
@@ -32,6 +15,74 @@ function writePassword(event) {
 
   passwordText.value = password;
 }
+
+// Code added by me
+
+function generatePassword() {
+  //Getting the password length
+  var passwordLength = prompt("Please choose the length of your password. (Must be 8 - 128 characters long)")
+  if (passwordLength >= 8 && passwordLength < 129) {
+    var passwordNum = passwordLength.value //How to store values??
+    lowercaseNum;
+    return '';
+    }
+  else {
+    var retry = alert("Password must be 8-128 characters.")
+    retry;
+    return 'Click "Generate Password" again';
+    }
+   //Incldue lowercase letters? 
+  var lowercaseNum = confirm("Would you like to include lowercase letters?")
+  if (lowercaseNum === true){
+    //Store data somehow
+    return '';
+  }
+  else {
+    //store data somehow
+  }
+  //Include uppercase letters?
+  var uppercaseNum = confirm("Would you like to include uppercase letters?")
+  if (uppercaseNum === true){
+    //Store data somehow
+    return '';
+  }
+  else {
+    //store data somehow
+  } 
+
+  //Include special characters?
+  var specialCharNum = confirm("Would you like to include special characters?")
+  if (specialCharNum  === true){
+    //Store data somehow
+    return '';
+  }
+  else {
+    //store data somehow
+  } 
+
+  //Include  numbers?
+  var numbersNum = confirm("Would you like to include numbers?")
+  if (numbersNum  === true){
+    //Store data somehow
+    return '';
+  }
+  else {
+    //store data somehow
+  } 
+
+  //To choose the choices they chose
+  var charChoices = [ , ]
+  
+  //Randomizing password
+  charChoices[Math.floor(Math.random() * choices[i].length)]
+
+  //For loop 
+  var randomPassword = ""
+  for (i=0; i<0; i++){
+
+  }
+  }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
